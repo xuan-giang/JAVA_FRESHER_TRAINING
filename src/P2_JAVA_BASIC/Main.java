773fa49af82dfa4fbe19d5ef2193faa4743020b3
XuanGiang <ngxuangiang2020@gmail.com>
2022-03-28 16:26:11 +0700

    2_JAVA_BASIC: Create project trainning

---
+++ src/P2_JAVA_BASIC/Main.java
@@ -0,0 +1,13 @@
+package P2_JAVA_BASIC;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // VARIABLES
+        Variables variables = new Variables();
+        variables.variableIs();
+
+
+
+    }
+}
---
+++ src/P2_JAVA_BASIC/Variables.java
@@ -0,0 +1,18 @@
+package P2_JAVA_BASIC;
+
+public class Variables {
+
+    // CONSTANT
+    final double PI = 3.14;
+
+    int age;
+    double average;
+    String fullName = "Nguyen Xuan Giang";
+    String message = "- Biến là vùng nhớ để lưu trữ giá trị và giá trị đó có thể thay đổi khi chương trình được thực thi.\n- Mỗi biến gắn liền với một kiểu dữ liệu và một định danh duy nhất được gọi là tên biến. ";
+
+    // What is variable?
+    public void variableIs()
+    {
+        System.out.println(this.message);
+    }
+}
